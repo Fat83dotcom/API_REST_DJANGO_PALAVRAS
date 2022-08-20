7357a9ec28af19855c1356702ebc5915ae5f1817
fat83dotcom <fernando.quimic@gmail.com>
2022-08-20 19:56:35 -0300

    alteração chave estrangeira tabela palavras

--- API_REST/models.py
+++ API_REST/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Palavras(models.Model):
     id_palavra = models.BigAutoField(primary_key=True)
     nome = models.CharField(max_length=255, unique=True)
-    categoria = models.CharField(max_length=255)
+    categoria = models.ForeignKey('CategoriaPalavra', on_delete=models.DO_NOTHING)
 
     def __str__(self) -> str:
         return self.nome
